c2de0755f06ff9ccbf12fe30f5ed5727be58a238
Jingjing Yang <jingjing.yang@tuni.fi>
2024-04-13 06:40:43 +0300

    play around with port calls api

--- msw-app/src/App.js
+++ msw-app/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 
 function App() {
   const [portCalls, setPortCalls] = useState(0);
+  const [firstPortCall, setFirstPortCall] = useState(0);
 
   useEffect(() => {
     fetch("https://meri.digitraffic.fi/api/port-call/v1/port-calls")
@@ -9,12 +10,17 @@ function App() {
       .then((data) => {
         setPortCalls(data.portCalls.length);
         console.log(`There are ${data.portCalls.length} portcalls.`);
+
+        setFirstPortCall(data.portCalls[0]);
       });
   }, []);
 
   return (
     <div>
       <p>There are {portCalls} portcalls.</p>
+      <hr />
+      <p>Example portcall:</p>
+      {firstPortCall && <pre>{JSON.stringify(firstPortCall, null, 4)}</pre>}
     </div>
   );
 }
