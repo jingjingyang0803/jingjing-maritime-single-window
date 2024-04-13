46e86daeefcfd5aa43aaee1d5aa365ea07da64c6
Jingjing Yang <jingjing.yang@tuni.fi>
2024-04-13 07:08:30 +0300

    play around with material UI

--- msw-app/src/App.js
+++ msw-app/src/App.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
+import { Container, Typography, Card, CardContent } from "@mui/material";
 
 function App() {
   const [portCalls, setPortCalls] = useState(0);
-  const [firstPortCall, setFirstPortCall] = useState(0);
+  const [firstPortCall, setFirstPortCall] = useState(null);
 
   useEffect(() => {
     fetch("https://meri.digitraffic.fi/api/port-call/v1/port-calls")
@@ -16,12 +17,22 @@ function App() {
   }, []);
 
   return (
-    <div>
-      <p>There are {portCalls} portcalls.</p>
-      <hr />
-      <p>Example portcall:</p>
-      {firstPortCall && <pre>{JSON.stringify(firstPortCall, null, 4)}</pre>}
-    </div>
+    <Container style={{ margin: "20px auto" }}>
+      <Typography variant="h3" gutterBottom>
+        There are {portCalls} portcalls.
+      </Typography>
+      {firstPortCall && (
+        <Card
+          variant="outlined"
+          className="MuiCard-root MuiCard-outlined"
+          style={{ backgroundColor: "#f5f5f5" }}
+        >
+          <CardContent>
+            <pre>{JSON.stringify(firstPortCall, null, 4)}</pre>
+          </CardContent>
+        </Card>
+      )}
+    </Container>
   );
 }
 
